43bbac9a2615bdc56e2cb3485efebcd2d9e4aa4e
lnson <leson131002@gmail.com>
2022-07-02 01:45:17 +0700

    first commit

---
+++ src/main/java/com/lnson/cicd/SpringbootGithubActionApplication.java
@@ -0,0 +1,14 @@
+package com.example.lnson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringbootGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGithubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lnson/SpringbootGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lnson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
